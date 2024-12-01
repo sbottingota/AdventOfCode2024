abdb0497faea2dde94d682a0b726d0790b8ee08c
sbottingota <saku.bottingota@gmail.com>
2024-12-01 08:24:52 +0000

    Wrote day 1 AOC

---
+++ day01_1.rs
@@ -0,0 +1,26 @@
+use std::cmp::{min, max};
+
+const FILE_PATH: &str = "./day1.txt";
+
+fn main() {
+    let mut left_list: Vec<u32> = Vec::new();
+    let mut right_list: Vec<u32> = Vec::new();
+
+    for line in std::fs::read_to_string(FILE_PATH).unwrap().lines() {
+        let mut line_iter = line.split_whitespace();
+        left_list.push(line_iter.next().unwrap().parse().unwrap());
+        right_list.push(line_iter.next().unwrap().parse().unwrap());
+    }
+
+    left_list.sort();
+    right_list.sort();
+
+    let mut differences_sum: u32 = 0;
+
+    for (left, right) in left_list.into_iter().zip(right_list) {
+        differences_sum += max(left, right) - min(left, right);
+    }
+
+    println!("{}", differences_sum);
+}
+
---
+++ day01_2.rs
@@ -0,0 +1,21 @@
+const FILE_PATH: &str = "./day1.txt";
+
+fn main() {
+    let mut left_list: Vec<u32> = Vec::new();
+    let mut right_list: Vec<u32> = Vec::new();
+
+    for line in std::fs::read_to_string(FILE_PATH).unwrap().lines() {
+        let mut line_iter = line.split_whitespace();
+        left_list.push(line_iter.next().unwrap().parse().unwrap());
+        right_list.push(line_iter.next().unwrap().parse().unwrap());
+    }
+
+    let mut similarity_score: u32 = 0;
+
+    for n in left_list {
+        similarity_score += n * right_list.iter().filter(|&&m| n == m).count() as u32;
+    }
+
+    println!("{}", similarity_score);
+}
+
