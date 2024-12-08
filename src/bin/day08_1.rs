dadfb06df117b3096d94f40ecf4bfbed7ba887b6
sbottingota <saku.bottingota@gmail.com>
2024-12-08 09:10:51 +0000

    Wrote day 8 code

---
+++ src/bin/day08_1.rs
@@ -0,0 +1,46 @@
+use std::collections::HashSet;
+
+const FILE_PATH: &str = "day08.txt";
+const EMPTY_SQUARE_CHAR: char = '.';
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+struct Antenna {
+    id: char,
+    pos: (isize, isize),
+}
+
+fn main() {
+    let mut antennas: Vec<Antenna> = Vec::new();
+
+    let input_str = std::fs::read_to_string(FILE_PATH).expect("Error opening input file");
+    let lines = input_str.lines();
+    let dims: (isize, isize) = (lines.clone().count() as isize, lines.clone().next().unwrap().len() as isize);
+
+    for (y, line) in lines.enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == EMPTY_SQUARE_CHAR {
+                continue;
+            }
+
+            antennas.push(Antenna { id: c, pos: (x as isize, y as isize) });
+        }
+    }
+
+    let mut antinodes: HashSet<(isize, isize)> = HashSet::new();
+
+    for antenna1 in &antennas {
+        for antenna2 in &antennas {
+            if antenna1 == antenna2 || antenna1.id != antenna2.id {
+                continue;
+            }
+
+            let dist = (antenna1.pos.0 - antenna2.pos.0, antenna1.pos.1 - antenna2.pos.1);
+
+            antinodes.insert((antenna1.pos.0 + dist.0, antenna1.pos.1 + dist.1));
+            antinodes.insert((antenna2.pos.0 - dist.0, antenna2.pos.1 - dist.1));
+        }
+    }
+
+    println!("{:?}", antinodes.into_iter().filter(|&(x, y)| x >= 0 && x < dims.0 && y >= 0 && y < dims.1).count());
+}
+
